0d6656967363e8caeb989e6b822095eb680f1085
Henrik Kiefer <hkln@gmx.net>
2024-02-27 12:56:06 +0100

    add force corr correction

--- multi_mems/multi_dim_gle.py
+++ multi_mems/multi_dim_gle.py
@@ -342,6 +342,8 @@ class multi_dim_gle:
                     else:
 
                         kernel_matrix.T[i][j]  = np.gradient(ikernel_matrix_half.T[i][j],self.dt)  
+                        
+            kernel_matrix.T[i][j][-1]*=0
 
         else:
         
